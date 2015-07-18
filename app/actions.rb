7ee216cec23778d82b69fc4a2920563be0d6de83
Carson Yong <CarsonYong@gmail.com>
2015-07-18 18:54:10 +0000

    Added message and new message page

--- app/actions.rb
+++ app/actions.rb
@@ -2,3 +2,20 @@
 get '/' do
   erb :index
 end
+
+# get '/messages' do
+#   'Message List here'
+# end
+
+# get '/messages' do
+#   erb :'messages/index'
+# end
+
+get '/messages' do
+  @messages = Message.all
+  erb :'messages/index'
+end
+
+get '/messages/new' do
+  erb :'messages/new'
+end
